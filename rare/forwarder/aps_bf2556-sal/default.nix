9771f9f5b0b9a6e141daa9e6c00d28a40d1f623c
Alexander Gall <alexander.gall@gmail.com>
2021-12-16 18:22:26 +0100

    Bump rare-bf2556x-1t to 8ebae7, fix issue with start_bfswd.sh
    8ebae7 contains a fix that lets salgrpcclient try to connect to the
    SAL GRPC port indefinitely to avoid timing issues in systemd services.
    
    start_bfswd.sh used to start the daemon with "exec". For some reason,
    this caused the bf-switchd systemd service to rerminate with SIGHUP
    when using the salRefApp for the stordis_bf2556x_1t platform.

--- rare/forwarder/aps_bf2556-sal/default.nix
+++ rare/forwarder/aps_bf2556-sal/default.nix
@@ -4,11 +4,11 @@ let
   fetchBitbucketPrivate = callPackage ./fetchbitbucket {};
 in python.pkgs.buildPythonPackage rec {
   pname = "sal-bf2556-t1";
-  version = "23c884";
+  version = "8ebae7";
   src = fetchBitbucketPrivate {
     url = "ssh://git@bitbucket.software.geant.org:7999/rare/rare-bf2556x-1t.git";
     rev = "${version}";
-    sha256 = "152zalxx32j3hkzdzgl1xm3grc2s9gp7ab6nzyfbgcka9yrlcg7v";
+    sha256 = "1ihxkfs5z350090x4da5cd0alh0axdj0rlgb7d6lk2339bjb7wa1";
   };
 
   preConfigure = ''
--- scripts/start_bfswd.sh
+++ scripts/start_bfswd.sh
@@ -3,4 +3,4 @@ set -e
 [ $# -eq 1 ]
 profile=$(cat $1)
 @CHECK_PROFILE@
-exec @WRAPPERS@/bin/bf_router_${profile}-module-wrapper
+@WRAPPERS@/bin/bf_router_${profile}-module-wrapper
