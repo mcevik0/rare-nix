9070f760078d0dad0e830e599f46c135fc2b30a3
release engineering <rare-dev@lists.geant.org>
2022-04-17 06:56:12 +0200

    bumping freerouter to 22.4.17
    qc pass: bridge port split horizon
    qc pass: bridge port unsplit horizon
    
    todo: tcpmss on bridges
    todo: allow command to be a substring of an other
    
    no todo: tcpmss on bridges
    
    todo: readvertise knob under routers
    
    todo: rt4/6import/export under vrfs
    
    no todo: rt4/6import/export under vrfs
    
    no todo: readvertise knob under routers
    
    qc pass: redistribution by prefix
    
    qc pass: redistribution with tag

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.4.12";
+  version = "22.4.17";
 
   src = fetchFromGitHub {
     owner = "mc36";
     repo = "freerouter";
-    rev = "38ab07";
-    sha256 = "1m79vvhqhyxiawkxvz4srx45gc8yvxi95kjz650yznh3wqy5pl9w";
+    rev = "7e3cd1b9c7833ccc096a8b852c6315505c00f8e5";
+    sha256 = "1gni96j2silq9afawsscqcx4y4silzh97dr9ga0bcipvrfc08kan";
   };
 }
