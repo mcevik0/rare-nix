ba08a65fdde2bf915a3546e31f015ce4a5f41bab
Alexander Gall <alexander.gall@gmail.com>
2022-04-21 10:31:31 +0200

    freerouter-native: support gcc and clang in build recipe

--- overlay/freerouter/native.nix
+++ overlay/freerouter/native.nix
@@ -1,12 +1,12 @@
-{ clangStdenv, fetchFromGitHub, libpcap, libbpf, libbsd, openssl,
+{ gcc10Stdenv, clang_10, fetchFromGitHub, libpcap, libbpf, libbsd, openssl,
   dpdk, numactl, makeWrapper, lib, iproute }:
 
 let
   repo = import ./repo.nix { inherit fetchFromGitHub; };
-in clangStdenv.mkDerivation (repo // {
+in gcc10Stdenv.mkDerivation (repo // {
   pname = "freerouter-native";
 
-  buildInputs = [ makeWrapper libpcap libbpf libbsd openssl dpdk numactl ];
+  buildInputs = [ makeWrapper libpcap libbpf libbsd openssl dpdk numactl clang_10 ];
 
   NIX_LDFLAGS = "-ldl -lnuma -lrte_telemetry -lrte_mbuf -lrte_kvargs -lrte_eal";
   NIX_CFLAGS_COMPILE = "-Wno-error=format-security";
