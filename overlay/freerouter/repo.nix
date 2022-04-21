c059cf652fa90fb30d98835a4b214e34df3b1cf7
Alexander Gall <alexander.gall@gmail.com>
2022-04-21 16:09:11 +0200

    Use new home of freeRtr on Github

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -4,8 +4,8 @@
   version = "22.4.17";
 
   src = fetchFromGitHub {
-    owner = "mc36";
-    repo = "freerouter";
+    owner = "rare-freertr";
+    repo = "freeRtr";
     rev = "7e3cd1b9c7833ccc096a8b852c6315505c00f8e5";
     sha256 = "1gni96j2silq9afawsscqcx4y4silzh97dr9ga0bcipvrfc08kan";
   };
