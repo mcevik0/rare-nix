a694ac9f19420e9d80f741327bbe99f9980ad077
release engineering <rare-dev@lists.geant.org>
2022-04-23 10:57:35 +0200

    bumping freerouter to 22.4.23

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.4.22";
+  version = "22.4.23";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "3612c357e60dd4b833b67d8d37f4ccf1e70b844c";
-    sha256 = "14i5bifb3i8w20fwgzaw3n8lv9y93g78jacrywwndd2picwz552d";
+    rev = "91e1004b14d41bfb49d6b528a8c25624e6d20ee1";
+    sha256 = "1nxcizbpybd28g6ni3b5n1hl4q1valp5h4i41m3j2q2pblz2nsys";
   };
 }
