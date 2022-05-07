7ef0879a921f92e90efac7fad85eb5841229c21d
mc36 <rare-dev@lists.geant.org>
2022-05-07 11:02:36 +0200

    bumping dataplane to 2022.05.07

--- rare/repo.nix
+++ rare/repo.nix
@@ -1,10 +1,10 @@
 { fetchgit }:
 
 {
-  version = "22.04.20";
+  version = "2022.05.07";
   src = fetchgit {
     url = "https://bitbucket.software.geant.org/scm/rare/rare.git";
-    rev = "65bbb8";
-    sha256 = "0702ry824sjnjd7rsb2dbm5qxvkdc2b6yvb2n17fqrplmq3wh2kc";
+    rev = "8bdd9fcdf48317c1ad975758b4ee6b834ddf1e35";
+    sha256 = "1j6zwfy95mcgqyv4zfiw5y96krc0if1vy5bsx2wsr4s6d9f4gbpn";
   };
 }
