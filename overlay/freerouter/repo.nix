7330b05651d4c88cd76608c6bb760717b356bc15
mc36 <rare-dev@lists.geant.org>
2022-05-07 09:40:42 +0200

    bumping freerouter to 22.5.7
    todo: sho run server

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.4.30";
+  version = "22.5.7";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "8951c75291460dd8b87a3646c04eb217d102f143";
-    sha256 = "1dysiq5mkr8grmk9qmlbg2mfg9h2zhbqfyyjqmbz6jjzs7532rxv";
+    rev = "f2849172fe88353d5c83b384ccaf10936d363368";
+    sha256 = "1r23hgnmgcb69734rzvn57r5k86y5slh2q5qjcw6ffgqlb5ib6ak";
   };
 }
