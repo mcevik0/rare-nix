03e4cf9d9189e4c4a3c7a9a4161b97f60d6cb418
mc36 <rare-dev@lists.geant.org>
2022-06-04 17:05:46 +0200

    bumping freerouter to 22.6.4
    qc pass: p4lang: mpls core over backplane
    qc pass: p4lang: mpls vpn over backplane

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.5.28";
+  version = "22.6.4";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "9dc4bd32634dd6bbdd280834290480a53a3ab50e";
-    sha256 = "0g8xm6kqklqf7gcac0l6lv2q9l2bx66xvs7qbn0sxkmq5v13y5qz";
+    rev = "9cb74447cfbb98e45d4b2c980692346307790d6d";
+    sha256 = "0867l7pnfa247bcml4y6qlr6nq6dmx7jy5l8a2ws0mlp657am9j2";
   };
 }
