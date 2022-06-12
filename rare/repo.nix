77719ea8ca62f297a6f99f71d36720af13afe272
mc36 <mc36@rare.mchome.nop.hu>
2022-06-12 07:00:32 -0400

    bumping dataplane to 2022.06.12

--- rare/repo.nix
+++ rare/repo.nix
@@ -1,10 +1,10 @@
 { fetchgit }:
 
 {
-  version = "2022.06.11";
+  version = "2022.06.12";
   src = fetchgit {
     url = "https://bitbucket.software.geant.org/scm/rare/rare.git";
     rev = "";
-    sha256 = "04pdkx9n1gp0wrg8svlq2py9s737c302gq8rwzqn6rza14f53frr";
+    sha256 = "0rz3lpahfs2npssxg6mzm1i10mx0s2xysgybb41yxpqygaqwagp8";
   };
 }
