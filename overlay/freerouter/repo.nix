20202cd5d0093070b91c32d88b2b4caec2b25de6
mc36 <mc36@rare.mchome.nop.hu>
2022-06-26 02:36:30 -0400

    bumping freerouter to 22.6.25

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -6,7 +6,7 @@
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "07ea7396fd2c9ca21e3305ccff3dbfbf5b8491ae";
-    sha256 = "1y4733a5ci1pxbj21l9rmk1zhqk72hs9g9mrpdq43f2symxp0ndv";
+    rev = "a0b70cbe4a19be211a92830a02b1ef9918de5754";
+    sha256 = "1rkpfii79cqs8jbc0lcxnnzc2pp3ksnq06rxr1w96wvlfk892c38";
   };
 }
