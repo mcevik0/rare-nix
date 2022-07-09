0e92da5d92afb22cce15aa0874c7c8f1e239044a
mc36 <mc36@rare.mchome.nop.hu>
2022-07-09 14:10:45 +0200

    bumping freerouter to 22.7.9
    qc pass: ebgp with backup peer
    qc pass: ibgp with backup peer
    qc pass: confed bgp with backup peer
    qc pass: route server bgp with backup peer
    
    todo: mpls static label mapping
    
    no todo: mpls static label mapping
    
    qc pass: static labels
    qc pass: static labels with ldp
    
    todo: ipv4/6 host-rate
    todo: pmtud tool
    
    todo: interface rate-limit-in/out
    
    no todo: interface rate-limit-in/out
    todo: test cases for interface rate-limit-in/out
    
    todo: arping
    
    no todo: ipv4/6 host-rate
    
    no todo: arping
    
    no todo: pmtud tool
    
    no todo: test cases for interface rate-limit-in/out
    qc pass: qos ingress policer action
    qc pass: qos egress policer action
    
    qc pass: pmtud
    qc pass: p4lang: pmtud
    
    qc pass: tcp mss in
    qc pass: tcp mss out
    
    no qc pass: pmtud
    qc pass: pmtud out
    qc pass: pmtud in
    
    qc pass: p4lang: vlan pmtud
    
    todo: test acl reports
    todo: test for ttl in ip and mpls, even in p4
    todo: test for p4 tcpmss
    todo: bridge pmtud
    todo: test for bridge pmtud and tcpmss, even in p4
    
    no qc pass: pmtud out
    
    no todo: test for p4 tcpmss
    qc pass: pmtud out
    qc pass: p4lang: tcpmss
    qc pass: p4lang: vlan tcpmss
    
    no todo: test acl reports
    
    no todo: test for ttl in ip and mpls, even in p4
    qc pass: ip ttl exceed
    qc pass: mpls ttl exceed
    
    no todo: test for bridge pmtud and tcpmss, even in p4
    no todo: bridge pmtud
    qc pass: bridge tcp mss in
    qc pass: bridge tcp mss out
    todo: bridge pmtud, even in p4
    
    qc pass: p4lang: bridge tcpmss
    qc pass: p4lang: vlan bridge tcpmss
    
    no todo: bridge pmtud, even in p4
    qc pass: bridge pmtud in
    qc pass: bridge pmtud out
    todo: bridge pmtud in p4
    
    no todo: bridge pmtud in p4
    qc pass: p4lang: bridge pmtud
    qc pass: p4lang: vlan bridge pmtud
    
    qc pass: p4lang: ip ttl exceed
    qc pass: p4lang: mpls ttl exceed

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.7.1";
+  version = "22.7.9";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "ea3ffb52c154c28b4786282cdbcf04caf0b448ae";
-    sha256 = "14mm4xs02wgm6mhxi06i4b5wgajzp4zi7ir4hz9ddz31qhaqnd6x";
+    rev = "98fe2c357141ae0190e957620382ceceaae2f0be";
+    sha256 = "0i6nljajpnwj7n1xrqn6dp5h4v8hn3rbabzw2qfvw2nxwml50x10";
   };
 }
