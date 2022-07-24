a45dff9926103841612d624fec98e283ada5c960
mc36 <mc36@rare.mchome.nop.hu>
2022-07-24 07:13:25 +0200

    bumping freerouter to 22.7.23
    qc pass: p4lang: multilink pppoe routing
    qc pass: p4lang: multilink pppoe mpls
    qc pass: p4lang: multilink l2tp routing over ipv4
    qc pass: p4lang: multilink l2tp routing over ipv6
    qc pass: p4lang: multilink l2tp routing over vlan
    qc pass: p4lang: multilink l2tp mpls over ipv4
    qc pass: p4lang: multilink l2tp mpls over ipv6
    qc pass: p4lang: multilink l2tp mpls over vlan
    
    qc pass: p4lang: multilink l2tp routing over bundle
    qc pass: p4lang: multilink l2tp mpls over bundle
    
    qc pass: dhcp with static allocation
    qc pass: dhcp with excluded address

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.7.16";
+  version = "22.7.23";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "43f47b5482a8a63c55dc2cadbdb8ff2143fc4460";
-    sha256 = "0w3sv7y48smzrqf12r58ik655dzb8pz6s2dw63fghrp2pmwrz94v";
+    rev = "537a00b88c9904ff2e4babb3804f4a0edafd3e3d";
+    sha256 = "1ckd1nni569sfgiswzfhy63axqhbjwvjyp3hcp0rd7vl65l6hfgd";
   };
 }
