124b6bae0823847a80e39ff5f00a56245a3b85ec
mc36 <mc36@rare.mchome.nop.hu>
2022-08-06 10:24:41 +0200

    bumping freerouter to 22.8.6
    qc pass: ingress fragment matching access list
    qc pass: ingress alert matching access list
    
    qc pass: bgp with ctp over sr
    qc pass: bgp with php ctp over sr
    qc pass: ebgp with ctp
    qc pass: ibgp with ctp
    qc pass: bgp additional path with ctp
    qc pass: ebgp with php ctp
    qc pass: ibgp with php ctp
    qc pass: bgp additional path with php ctp
    qc pass: ibgp rr with ctp
    qc pass: confed bgp with ctp
    qc pass: bgp route server with ctp
    qc pass: bgp soft-reconfig with ctp
    qc pass: ctp bgp ingress route filtering with prefixlist
    qc pass: ctp bgp egress route filtering with prefixlist
    qc pass: ctp bgp ingress route filtering with routemap
    qc pass: ctp bgp egress route filtering with routemap
    qc pass: ctp bgp ingress route filtering with prefixlist with soft-reconfig
    qc pass: ctp bgp egress route filtering with prefixlist with soft-reconfig
    qc pass: ctp bgp ingress route filtering with routemap with soft-reconfig
    qc pass: ctp bgp egress route filtering with routemap with soft-reconfig
    qc pass: ctp bgp ingress route filtering with routepolicy
    qc pass: ctp bgp egress route filtering with routepolicy
    qc pass: ctp bgp ingress route filtering with routepolicy with soft-reconfig
    qc pass: ctp bgp egress route filtering with routepolicy with soft-reconfig
    qc pass: ctp bgp default route
    qc pass: ctp bgp aggregation
    qc pass: bgp change in metric with ctp
    qc pass: ibgp rr prefix movement with ctp
    qc pass: ibgp rr prefix withdraw with ctp
    qc pass: unicast+octp over ebgp
    qc pass: unicast+octp over bgp with soft-reconfig
    qc pass: unicast+octp over bgp with additional path
    qc pass: unicast+octp over bgp route server
    qc pass: unicast+octp over ibgp
    qc pass: unicast+octp over ibgp rr
    qc pass: unicast+octp over confed bgp
    qc pass: octp over srv6 over ibgp
    qc pass: octp over srv6 over ebgp
    qc pass: octp over srv6 over ibgp rr
    qc pass: octp over srv6 over confed bgp
    qc pass: octp over srv6 over bgp route server
    qc pass: octp over srv6 over bgp with soft-reconfig
    qc pass: octp over srv6 over bgp additional path
    qc pass: octp bgp default route
    qc pass: octp bgp ingress route filtering with prefixlist
    qc pass: octp bgp egress route filtering with prefixlist
    qc pass: octp bgp ingress route filtering with routemap
    qc pass: octp bgp egress route filtering with routemap
    qc pass: octp bgp ingress route filtering with prefixlist with soft-reconfig
    qc pass: octp bgp egress route filtering with prefixlist with soft-reconfig
    qc pass: octp bgp ingress route filtering with routemap with soft-reconfig
    qc pass: octp bgp egress route filtering with routemap with soft-reconfig
    qc pass: octp bgp ingress route filtering with routepolicy
    qc pass: octp bgp egress route filtering with routepolicy
    qc pass: octp bgp ingress route filtering with routepolicy with soft-reconfig
    qc pass: octp bgp egress route filtering with routepolicy with soft-reconfig
    qc pass: octp bgp aggregation
    qc pass: octp bgp change in metric
    qc pass: octp ibgp rr prefix movement
    qc pass: octp ibgp rr prefix withdraw
    qc pass: octp ibgp fullmesh prefix withdraw
    qc pass: olab+octp over ebgp
    qc pass: redistribution with bgp ctp
    qc pass: redistribution with octp bgp
    
    qc pass: interop9: bgp with ctp
    qc pass: interop9: bgp with php ctp

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.7.30";
+  version = "22.8.6";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "556f3f108fe644681aac0adb6e533e844f47361e";
-    sha256 = "1952jlrbzprnzfx6q15jb5pf79yan3az7ssizzf0kd5541ab8k35";
+    rev = "640f4da03bafd55d051d30f76ae0c443736a8f8b";
+    sha256 = "1vdpfnsxrwsp0cn0ipmv5h26hbc5wf37aily3gwciawj698qd3cf";
   };
 }
