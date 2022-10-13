d42ca614421360103c8e012a473b2bc16513f817
Alexander Gall <alexander.gall@gmail.com>
2022-10-13 10:09:42 +0200

    Move RARE repo to 09e14ee
    Pick up a workaround for a problem affecting Tofino2.

--- rare/repo.nix
+++ rare/repo.nix
@@ -1,10 +1,10 @@
 { fetchgit }:
 
 {
-  version = "2022.09.25";
+  version = "2022.10.13";
   src = fetchgit {
     url = "https://bitbucket.software.geant.org/scm/rare/rare.git";
-    rev = "2bd114a47dbb4a5b5c4511a7601aa2966afd238d";
-    sha256 = "0q0bls272bqpyxkc3paqcgbgpj8lzr417zznsi5kayv8pskl6n52";
+    rev = "09e14ee49d419753b966bdab4d55346ee6b11daf";
+    sha256 = "0wza0p6r7lxslclv7kxm6a2d194y4nl2fk9vakwh95s5z6glvxp8";
   };
 }
