9bb4ece8edaf4fa5251c564fb57d9c2a87332419
mc36 <mc36@rare.mchome.nop.hu>
2022-10-23 08:18:54 +0200

    bumping freerouter to 22.10.22

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.10.15";
+  version = "22.10.22";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "5cfe80da0fef8c308dab16a72c1bf23c1f8347f4";
-    sha256 = "19jxvw68lpxh9l3cb265y5i8a4br7m384n1msj5p0j1qashjgnx2";
+    rev = "80d52caf2c10dc228caa0b243f18b88933850c0c";
+    sha256 = "1z1n946ccgjxb0n3s4a4y2lw62brklidi7s20nx8zy05jprrnsk7";
   };
 }
