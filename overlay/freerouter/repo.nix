d93620904af1ba9d28d7fb907db5365fcc776dea
mc36 <mc36@rare.mchome.nop.hu>
2022-12-16 18:04:42 +0100

    bumping freerouter to 22.12.16
    no todo: rift4, rift6
    
    qc pass: interop9: point2point rift
    qc pass: rift point2point connection
    qc pass: rift point2point chain
    qc pass: rift metric
    qc pass: rift address suppression
    qc pass: rift route filtering with prefixlist
    qc pass: rift route filtering with routemap
    qc pass: rift route filtering with routepolicy
    qc pass: rift tag
    qc pass: rift default route
    qc pass: rift with bfd
    qc pass: rift peer template
    qc pass: rift prefix withdraw
    qc pass: rift change in metric
    qc pass: rift change in tag
    qc pass: rift auto mesh tunnel
    qc pass: rift triangle connection
    qc pass: rift prefix movement
    qc pass: rift autoroute
    qc pass: rift default address suppression
    qc pass: rift address unsuppression
    qc pass: rift over point2point ethernet
    qc pass: rift point2point connection with bidir check
    qc pass: rift ecmp connection
    
    no todo: fix enter in spacetab term behaviour
    
    qc pass: rift with bgp linkstate
    qc pass: rift inter level chain
    
    no todo: allowed v4/v6 range to temper
    
    no todo: xot, x75 switching
    
    no todo: x25
    
    qc pass: xot pad
    qc pass: rift dynamic icmp metric
    qc pass: rift dynamic udp metric
    qc pass: rift dynamic twamp metric

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.11.26";
+  version = "22.12.16";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "e48b2c9593c7662d9531c2dabe5f99c990c4ee93";
-    sha256 = "0nyjng4105kpkxhlzlv6p3wksivj909ldjprvis5qf7wf8bg2n7c";
+    rev = "fc261bd0181caad2aad70aa6f26ad50a773e0674";
+    sha256 = "0yvcsf06rsmgwsg85h7idqp6cqg2x4r3v4adh2lbvdzyq01bg0ik";
   };
 }
