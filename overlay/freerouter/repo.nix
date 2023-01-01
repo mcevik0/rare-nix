a9ab55546570c0ad6c626d4492f8c85a1ec9cf83
mc36 <mc36@rare.mchome.nop.hu>
2023-01-01 05:11:01 +0100

    bumping freerouter to 22.12.31
    no todo: colorful spf class
    no qc pass: lsrp database authentication
    failed: lsrp database authentication
    
    qc pass: lsrp flexalgo
    
    qc pass: ospf flexalgo
    
    qc pass: isis flexalgo
    qc pass: integrated isis address unsuppression
    
    no failed: lsrp database authentication
    no qc pass: integrated isis address unsuppression
    qc pass: integrated isis flexalgo
    qc pass: lsrp database authentication
    
    no todo: bgp strict bfd mode
    
    qc pass: ebgp with strict bfd
    
    qc pass: ebgp with asymmetric bfd
    
    todo: bgp route policy distribution
    
    no todo: bgp route policy distribution
    qc pass: unicast+rpd over bgp
    qc pass: unicast+rpd over bgp with soft-reconfig
    qc pass: unicast+rpd over bgp with additional path
    
    todo: policy from bgp rpd
    
    qc pass: legacy bgp session

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "22.12.24";
+  version = "22.12.31";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "3d1540793ef82fa7a3897d64a984070e3bb6e1aa";
-    sha256 = "1npr83vhfch5iz3lszgdkdfbgj84cwqqvz8pmna666qai0dq1r6d";
+    rev = "3f47e64cfc3aa8dded35a28e9ee9f06cd3f6a37d";
+    sha256 = "0zmks0bfl29hbwn68z0xxfljv9h561shmmp9my1n7bbafbz2l0h6";
   };
 }
