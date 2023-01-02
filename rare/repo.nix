c0e1421dcfda1c7e549a38b8d7a9dc9e20e606e0
Alexander Gall <alexander.gall@gmail.com>
2023-01-02 14:08:51 +0100

    Update rare repo to e1d874
    Update optimized profiles.

--- rare/repo.nix
+++ rare/repo.nix
@@ -1,10 +1,10 @@
 { fetchgit }:
 
 {
-  version = "2023.01.01";
+  version = "2023.01.02";
   src = fetchgit {
     url = "https://bitbucket.software.geant.org/scm/rare/rare.git";
-    rev = "1f1243af19fe044ae0906e78d68deeabf38682bd";
-    sha256 = "1gb7vcggzb94hi9lkxaisw2c9wl2843pls698gjzbrckqnw653av";
+    rev = "e1d8740162dbaafec9ebd5a5ae23c5c90f56b062";
+    sha256 = "sha256-HxBm1A1Dnr2oEyGXU6N4BsoXP+Sgro8Oana05eQBVPk=";
   };
 }
