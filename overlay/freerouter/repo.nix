402d28beb8356ff8ea3f7bba86b5cdf1349e1bb4
mc36 <mc36@rare.mchome.nop.hu>
2023-01-28 11:02:41 +0100

    bumping freerouter to 23.1.27
    qc pass: bgp interas labels with multiple labels
    qc pass: bgp interas ctp with multiple labels

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "23.1.21";
+  version = "23.1.27";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "bd33f45e195214a7d35bb579e284fe8280e3dd38";
-    sha256 = "015ix3im8cm3ym03qsbqpypamhpysazdmf1d87sc0a6i7kidlb2j";
+    rev = "e9e5aa9809ed4662bfc8e7216af7802f4b8b9aa9";
+    sha256 = "0p8790pk19grn2wgcmhw432vrf34b050fnjxsawpqfwm87gdjy6y";
   };
 }
