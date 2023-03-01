20d7cebfd0fc260d369b19e0533dde1357b7eb35
Alexander Gall <alexander.gall@gmail.com>
2023-03-01 15:06:04 +0100

    Add support for Asterfusion

--- rare/platforms.nix
+++ rare/platforms.nix
@@ -4,9 +4,10 @@
   accton_wedge100bf_65x = [ "-DQUAD_PIPE" ];
   accton_as9516_32d = [ "-DQUAD_PIPE" ];
   inventec_d10064 = [ "-DQUAD_PIPE" ];
-  stordis_bf2556x_1t = [ "-DDUAL_PIPE" ];
-  stordis_bf6064x_t = [ "-DQUAD_PIPE" ];
-  netberg_aurora_710 = [ "-DDUAL_PIPE" ];
+  asterfusion_x308p = [ "-DDUAL_PIPE" ];
+  asterfusion_x312p = [ "-DDUAL_PIPE" ];
+  asterfusion_x532p = [ "-DDUAL_PIPE" ];
+  asterfusion_x564p = [ "-DQUAD_PIPE" ];
   model = [];
   modelT2 = [];
 }
