cab62f9cb9f85b417b7fd45bb6c5a1a19e48b9cf
mc36 <mc36@rare.mchome.nop.hu>
2023-03-05 21:37:35 +0100

    bumping freerouter to 23.3.5
    no todo: consider term caps
    
    todo: vnc console to the vdc definition
    todo: boot order to vdc definition
    
    todo: dn42ix-vm-switch#terminal no tablemode [fancy]--- % unknown exec command entered
    
    todo: fuckup from yesterday: the colsole times out now, go for it asap you crazy fool XD
    
    no todo: fuckup from yesterday: the colsole times out now, go for it asap you crazy fool XD
    
    no todo: boot order to vdc definition
    no todo: dn42ix-vm-switch#terminal no tablemode [fancy]--- % unknown exec command entered
    
    no todo: vnc console to the vdc definition
    
    todo: pastebin for the cli
    
    todo: server http and : in the file names
    
    no todo: server http and : in the file names
    
    todo: noflash -hw.txt knob, automatically on for vdcs
    
    no todo: noflash -hw.txt knob, automatically on for vdcs
    
    todo: send ttyX exec command
    
    no todo: send ttyX exec command
    
    todo: terminal background and foreground
    
    no todo: pastebin for the cli
    todo: curl
    
    todo: afi-vrf asdf setvrf
    todo: asn for remote-as -1 in shows
    
    no todo: afi-vrf asdf setvrf
    todo: test for afi-vrf asdf setvrf

--- overlay/freerouter/repo.nix
+++ overlay/freerouter/repo.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "23.2.18";
+  version = "23.3.5";
 
   src = fetchFromGitHub {
     owner = "rare-freertr";
     repo = "freeRtr";
-    rev = "a57c206c73d6614cf59f9f7ed42822b962981686";
-    sha256 = "0lliy110ssibjf3d8igw8sy25b9adkk3a4axj4lbrdi41ml0i3c2";
+    rev = "53d029666ff19fe43658c4c4d3319f613c80d97c";
+    sha256 = "0darq6m6iibdwzcxqp2jv5hpg3dcsvziafwchlva276jli5mp7sb";
   };
 }
