952aa018f69fdb7f9b7fff1e6b82b97ce931d1b9
mc36 <mc36@rare.mchome.nop.hu>
2023-03-12 08:51:56 +0100

    bumping dataplane to 2023.03.12

--- rare/repo.nix
+++ rare/repo.nix
@@ -1,7 +1,7 @@
 { fetchgit }:
 
 {
-  version = "2023.03.05";
+  version = "2023.03.12";
   src = fetchgit {
     url = "https://bitbucket.software.geant.org/scm/rare/rare.git";
     rev = "a88c7397109a4f8d7d2ffc99e76b9e551f394f61";
